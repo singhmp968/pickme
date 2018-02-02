3f73973b440e4ebe23b80ae2242c8f052b58b4ef
ganemcos <ganemcos@gmail.com>
2018-02-02 11:23:12 +0530

    Initial commit

---
+++ app/src/main/java/com/sportsme/saiganesh/pickme/ChoosemodeActivity.java
@@ -0,0 +1,13 @@
+package com.sportsme.saiganesh.pickme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChoosemodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choosemode);
+    }
+}
