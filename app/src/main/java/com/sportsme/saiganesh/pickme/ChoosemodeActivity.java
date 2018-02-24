069f9470cdc50f8b75891e75df7cd0c093ab8254
ganemcos <ganemcos@gmail.com>
2018-02-24 22:33:27 +0530

    signup to firebase created

--- app/src/main/java/com/sportsme/saiganesh/pickme/ChoosemodeActivity.java
+++ app/src/main/java/com/sportsme/saiganesh/pickme/ChoosemodeActivity.java
@@ -1,7 +1,10 @@
 package com.sportsme.saiganesh.pickme;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChoosemodeActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class ChoosemodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choosemode);
+
+    }
+
+    public void signinFunction(View view)
+    {
+
+        SharedPreferences sharedPreferences=getSharedPreferences("PREFS",0);
+        String count=sharedPreferences.getString("COUNT","");
+
+        if (!count.equals("YES")) {
+
+            Intent abc = new Intent(this, signup.class);
+            startActivity(abc);
+        }
+        else {
+            startActivity(new Intent(ChoosemodeActivity.this,LoginActivity.class));
+        }
     }
 }
---
+++ app/src/main/java/com/sportsme/saiganesh/pickme/signup.java
@@ -0,0 +1,48 @@
+package com.sportsme.saiganesh.pickme;
+
+        import android.content.SharedPreferences;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.Toast;
+
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+
+public class signup extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    EditText emal,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        mAuth = FirebaseAuth.getInstance();
+
+        emal=(EditText)findViewById(R.id.email);
+        password=(EditText)findViewById(R.id.password);
+
+        findViewById(R.id.btnsignup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.createUserWithEmailAndPassword(emal.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        SharedPreferences sharedPreferences=getSharedPreferences("PREFS",0);
+                        SharedPreferences.Editor editor=sharedPreferences.edit();
+                        editor.putString("COUNT","YES");
+                        Toast.makeText(signup.this, "user Created", Toast.LENGTH_SHORT).show();
+                        editor.apply();
+                    }
+                });
+            }
+        });
+       
+    }
+}
